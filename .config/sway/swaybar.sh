f690144b75b5e7a540b92399c4be59c249f01631
Ram-Delos-Santos <ramjesler.delossantos@gmail.com>
2025-08-28 23:22:28 +0800

    Added CPU temps to swaybar

--- .config/sway/swaybar.sh
+++ .config/sway/swaybar.sh
@@ -4,6 +4,7 @@ while true; do
     date=$(date +'%A, %B %d  %I:%M %p')
     weather=$(curl -s 'wttr.in/Rizal?format=%c+%t')
     wifi=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d':' -f2)
-    echo " $weather | $wifi | $date "
+    cpu=$(sensors | grep 'Tctl' | awk '{print $2}')
+    echo " $cpu | $weather | $wifi | $date "
     sleep 1
 done
