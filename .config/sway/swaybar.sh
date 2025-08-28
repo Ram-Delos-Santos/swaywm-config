974dfc82a60645bcc10cdb0135ecda261112dce6
Ram-Delos-Santos <ramjesler.delossantos@gmail.com>
2025-08-28 23:23:50 +0800

    Added icon

--- .config/sway/swaybar.sh
+++ .config/sway/swaybar.sh
@@ -5,6 +5,6 @@ while true; do
     weather=$(curl -s 'wttr.in/Rizal?format=%c+%t')
     wifi=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d':' -f2)
     cpu=$(sensors | grep 'Tctl' | awk '{print $2}')
-    echo " $cpu | $weather | $wifi | $date "
+    echo "  $cpu | $weather | $wifi | $date "
     sleep 1
 done
